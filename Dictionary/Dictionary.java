1708ec8e7f0247e55580d8e7bde5ffa2064d8dc3
OIsmail99 <osama.m.esmael999@gmail.com>
2024-12-26 11:19:01 +0200

    Update Dictionary

--- Dictionary/Dictionary.java
+++ Dictionary/Dictionary.java
@@ -1,30 +1,37 @@
 import java.util.*;
 
-import javafx.scene.control.TreeCell;
-
 class Dictionary {
 
-    static TreeMap<Character, TreeSet<String>> dictionary = new TreeMap<>();
+    static TreeMap<Character, TreeMap<String, String>> dictionary = new TreeMap<>();
 
-    public static void addWord(String word) {
-        Character firstLetter = Character.toUpperCase(word.charAt(0));
-        TreeSet<String> words = dictionary.get(firstLetter);
-        words.add(word);
+    public static void addWord(String word, String definition) {
+        char letter = word.charAt(0);
+        TreeMap<String, String> wordAndDefinition = dictionary.get(letter);
+        wordAndDefinition.put(word, definition);
     }
 
     public static void print() {
         System.out.println(dictionary);
     }
 
+    public static void printWordsOfLetter(char letter) {
+        System.out.println(dictionary.get(Character.toUpperCase(letter)));
+    }
+
     public static void main(String[] args) {
+
         String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         char[] letterArray = letters.toCharArray();
-        for (int i = 0; i < letters.length(); i++) {
-            dictionary.put(letterArray[i], new TreeSet<String>());
+        for (int i = 0; i < letterArray.length; i++) {
+            dictionary.put(letterArray[i], new TreeMap<String, String>());
         }
-        addWord("Apple");
-        addWord("letters");
-        addWord("FullFree");
+
+        addWord("Apple", "A fruit that prevents you from going to the doctor");
+        addWord("Add", "A verb which I don't know its meaning");
+
+        addWord("FullFree", "My favorite word in the dictionary");
+
+        printWordsOfLetter('a');
         print();
     }
 }
